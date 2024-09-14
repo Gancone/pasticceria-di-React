12865b6d8a9e076e666990f86d50d7a74f6b68d7
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2024-09-14 14:32:03 +0200

    ridimensionato textarea e reso funzionante il form

--- src/components/Contattaci.js
+++ src/components/Contattaci.js
@@ -2,26 +2,27 @@ import React from 'react';
 
 const Contattaci = () => {
     return (
-        <form className='container-main-form' >
+        <form className='container-main-form' action="https://formspree.io/f/meojwlga"
+            method="POST">
             <div className='container-form'>
                 <div className='container-contattaci'>
                     <label className='contattaci-label'>Nome</label>
-                    <input type='text' placeholder='Il tuo nome' />
+                    <input type='text' placeholder='Il tuo nome' name='name' />
 
                 </div>
 
                 <div className='container-contattaci'>
                     <label className='contattaci-label'>Cognome</label>
-                    <input type='text' placeholder='Il tuo cognome' />
+                    <input type='text' placeholder='Il tuo cognome' name='surname' />
                 </div>
                 <div className='container-contattaci'>
                     <label className='contattaci-label'>Email</label>
-                    <input type='email' placeholder='La tua email' />
+                    <input type='email' placeholder='La tua email' name='email' />
 
                 </div>
                 <div className='container-contattaci'>
                     <label className='contattaci-label'>Descrizione</label>
-                    <textarea type='text' placeholder='Inserisci qui la descrizione' rows={10}></textarea>
+                    <textarea type='text' placeholder='Inserisci qui la descrizione' rows={10} name='description'></textarea>
 
                 </div>
                 <button type='submit'>Invia</button>
